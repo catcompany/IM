df681349e0b4c8e196aa6afb65a106982d0d0f5d
iMorning <admin@catcompany.cn>
2021-04-19 23:23:40 +0800

    add C++

---
+++ app/src/main/jni/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by iMorning on 2021-04-19.
+//
+
+#include "main.h"
---
+++ app/src/main/jni/main.h
@@ -0,0 +1,16 @@
+//
+// Created by iMorning on 2021-04-19.
+//
+
+#ifndef IM_MAIN_H
+#define IM_MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //IM_MAIN_H
